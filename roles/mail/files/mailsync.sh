c4938ea9bde51c8b3b6ef16d2368256d5c19404c
Pig Monkey <pm@pig-monkey.com>
2015-12-25 19:52:44 -0800

    send and receive with mailsync.sh

--- roles/mail/files/mailsync.sh
+++ roles/mail/files/mailsync.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # Original source: http://bit.ly/1Ztk5wg
-# Run mbsync when full internet connectivity is available.
+# Send and receive mail when full internet connectivity is available.
 
 STATE=`nmcli networking connectivity`
 
 if [ $STATE = 'full' ]
 then
+    /usr/local/bin/msmtp-queue -r
     /usr/bin/mbsync -a
     exit $?
 fi
