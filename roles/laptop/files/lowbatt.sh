48c3184c9b582d0e7b446950edef4c3328c3bd11
Pig Monkey <pm@pig-monkey.com>
2016-01-28 22:25:29 -0800

    refactor low battery handling
    udev was not distinguishing between multiple batteries. It was
    suspending the system when the secondary battery reached the threshold,
    even if the primary battery was still full, which is not the desired
    behaviour.
    
    There may be a way to tell udev which battery we care about, but it was
    easier for me to just write a shell script that handles all the logic.
    The script warns when the primary battery is low and suspends when the
    primary battery is critical.
    
    I tried calling the script with a single udev rule with a capacity range
    of 0-10, however that resulted in no warnings and suspending only when
    the battery went to 0%. Not useful. With two rules with different ranges
    like this, the system is consistently suspended at 5%, which is what I
    want. I sometimes get warnings in the 6-10% range, but not consistently.
    I don't know why.
    
    If the warnings won't work, we could create a systemd timer to call the
    script every ~60 seconds or so. That will definitely work, but it seems
    like udev should be able to handle this.

--- roles/laptop/files/lowbatt.sh
+++ roles/laptop/files/lowbatt.sh
@@ -1,15 +1,39 @@
 #!/bin/sh
 
-capacity='low'
+BATTERY="BAT0"
 
-# Try to get an actual percentage.
-if [ -f /sys/class/power_supply/BAT0/capacity ]; then
-    capacity="at `cat /sys/class/power_supply/BAT0/capacity`%"
+low() {
+    # Warn if the battery is low.
+    message="Battery $BATTERY is at $CAPACITY. We need more power, Scotty!"
+    systemd-cat -t 'lowbatt' -p warning echo "$message"
+    notify-send --urgency=critical "Low Battery" "$message"
+    wall "Battery is low. $message"
+}
+
+critical() {
+    # Suspend if the battery is critical.
+    systemd-cat -t 'lowbatt' -p warning echo "Battery $BATTERY is critical. Suspending."
+    /usr/bin/systemctl suspend
+}
+
+# Allow the user to specify a different battery.
+if [ -n "$1" ]; then
+    BATTERY="$1"
 fi
 
-# Build the message body.
-message="Battery is $capacity. We need more power, Scotty!"
+# Only continue if we can get the capacity of the battery.
+if [ -f /sys/class/power_supply/"$BATTERY"/capacity ]; then
+    # Get the remaining capacity of the battery.
+    CAPACITY=`cat /sys/class/power_supply/"$BATTERY"/capacity`
+else
+    echo "Could not get capacity for battery $BATTERY."
+    exit 1
+fi
 
-# Make some noise.
-notify-send --urgency=critical "Low Battery" "$message"
-wall "Battery is low. $message"
+# If the capacity is between 5 and 10, it is low.
+if [ "$CAPACITY" -gt 5 -a "$CAPACITY" -le 10 ]; then
+    low
+# If the capacity is 5 or less, it is critical.
+elif [ "$CAPACITY" -le 5 ]; then
+    critical
+fi
