c29f7c22df39e4099247e3edc7eec99a61177052
Pig Monkey <pm@pig-monkey.com>
2016-04-15 17:56:41 -0700

    add play script for mpv + youtube-dl integration
    I recommend binding play to a key via sxhkd. Enjoy streaming video from
    anywhere, without allowing all the javascripts in your browser.

---
+++ roles/mpv/files/play.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Take advantage of mpv's youtube-dl integration to easily watch videos outside
+# of the browser. Use sxhkd or xbindkeys to bind this script to a key. When
+# you're on a YouTube page, copy the URL to your clipboardand and hit your key
+# binding; the video will automatically begin streaming via mpv.
+###############################################################################
+
+read -t 0 stdin
+
+if [ -n "$stdin" ]; then
+    source="$stdin"
+elif [ -n "$1" ]; then
+    source="$1"
+else
+    source=$(xclip -o -selection clipboard)
+fi
+
+eval mpv "$source"
